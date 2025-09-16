ce6057d646b51d451624aab1204944402ae3f5d7
adipukr <dipu.kumar@50hertz.in>
2025-09-16 15:21:33 +0530

    chg

--- authservice/src/main/java/main/Main.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		AuthServer authServer = new AuthServer(8780);
-		authServer.start();
-	}
-}
--- http/src/main/java/main/Handler.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-@FunctionalInterface
-public interface Handler {
-	void handle(Request req, Response res) throws Exception;
-}
